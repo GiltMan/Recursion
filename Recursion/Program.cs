db8a3236bd1ccfb0e6a020f67004dc81df155677
Matthew Quick <ussmwq@gmail.com>
2016-03-03 08:33:03 -0600

    First Commit
    Testing VS.gitignore

---
+++ Recursion/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start("D:\\Recursion.exe");
+            for (int i = 0; i < 5; i++)
+            {
+                System.Console.WriteLine("Hi");
+            }
+
+        }
+    }
+}
